12664bc64946b7b9b7a5f8fa0353302fdd8382ea
Valery Mikhailov <va1eonmix@gmail.com>
2022-10-04 13:02:49 +0900

    add functions(mixins) in scss, webp support script and add normalize.css, fucus-visible in package.json on devDep field

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
+import 'focus-visible';
+import {isWebp} from './modules/isWebp.js';
+
+// CHECK SUPPORT WEBP FORMAT
+(async () => {
+  (await isWebp())
+    ? document.querySelector('html').classList.add('webp')
+    : document.querySelector('html').classList.add('no-webp');
+})();
 import {sum} from './modules/test.js';
 
 const inner = document.querySelector('#inner');
